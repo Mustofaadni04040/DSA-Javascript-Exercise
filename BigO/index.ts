95608c19a18ee08f7591fd0999a1cde0e9638c73
mustofa adny <adniadni04@gmail.com>
2025-11-21 15:00:27 +0700

    O(n): Linear time complexity

--- BigO/index.ts
+++ BigO/index.ts
@@ -1,4 +1,14 @@
+// O(1)/Constant: One time complexity
 function multiplyNumber(n: number): number {
   return n * n;
 }
 console.log(multiplyNumber(10));
+
+// O(n)/Linear: Linear time complexity
+// whenever the number is increased, the time complexity is increased
+function printItems(n: number) {
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+}
+console.log(printItems(10));
