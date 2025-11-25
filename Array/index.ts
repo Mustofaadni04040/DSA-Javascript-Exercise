89561589569d265f39cd048f7f21e8a55e1d570e
mustofa adny <adniadni04@gmail.com>
2025-11-25 12:03:47 +0700

    inserting element to array

--- Array/index.ts
+++ Array/index.ts
@@ -1,9 +1,17 @@
 // array literal syntax | time complexity: O(1) | space complexity: O(1) if array is empty, otherwise O(n)
 const arr: number[] = [1, 2, 3, 4, 5];
 
-console.log(arr);
+// console.log(arr);
 
 // array constructor syntax | time complexity: O(1) | space complexity: O(n)
 const arr2 = new Array(10);
 
-console.log(arr2);
+// console.log(arr2);
+
+const arr3: number[] = [1, 2, 3, 4, 5];
+
+arr3.push(6);
+arr3.unshift(0); // time complexity: O(n) | space complexity: O(1)
+arr3.splice(2, 0, 9); // time complexity: O(n) | space complexity: O(1)
+
+console.log(arr3);
